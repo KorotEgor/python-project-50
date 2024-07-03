c130009986347788c57a9c173861179d12ed7eab
korotkov_egor <korotkovegor10@gmail.com>
2024-07-03 20:30:01 +0300

    task 2

--- difference_calculator/scripts/gendiff.py
+++ difference_calculator/scripts/gendiff.py
@@ -8,9 +8,11 @@ def show_certificate():
     parser = argparse.ArgumentParser(description=desc)
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument('-f', '--format',
+            help='set format of output')
 
     args = parser.parse_args()
-    print(args.filename, args.filename)
+    print(args.first_file, args.second_file, args.format)
 
 
 if __name__ == '__main__':
