771d1e65267b534ea60fe1f1490945f6949e8033
korotkov_egor <korotkovegor10@gmail.com>
2024-07-03 22:42:34 +0300

    task 3

--- difference_calculator/scripts/gendiff.py
+++ difference_calculator/scripts/gendiff.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 
 import argparse
+import json
+
+
+def generate_diff(path1, path2):
+    file1 = json.load(open(path1))
+    file2 = json.load(open(path2))
+
+    keys = sorted(list(file1 | file2))
+    status = ['{']
+    for el in keys:
+        if el in file1 and el in file2 and file1[el] == file2[el]:
+            status.append(f'    {el}: {json.dumps(file1[el])}')
+        elif el in file1 and el in file2 and file1[el] != file2[el]:
+            status.append(f'  - {el}: {json.dumps(file1[el])}')
+            status.append(f'  + {el}: {json.dumps(file2[el])}')
+        elif el in file1 and el not in file2:
+            status.append(f'  - {el}: {json.dumps(file1[el])}')
+        else:
+            status.append(f'  + {el}: {json.dumps(file2[el])}')
+    status.append('}')
+    return '\n'.join(status)
 
 
 def show_certificate():
@@ -8,11 +29,10 @@ def show_certificate():
     parser = argparse.ArgumentParser(description=desc)
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument('-f', '--format',
-            help='set format of output')
+    parser.add_argument('-f', '--format', help='set format of output')
 
     args = parser.parse_args()
-    print(args.first_file, args.second_file, args.format)
+    print(generate_diff(args.first_file, args.second_file))
 
 
 if __name__ == '__main__':
