6c7bc5144687764c90e08c737b7b144905c186a9
korotkov_egor <korotkovegor10@gmail.com>
2024-07-23 21:52:12 +0300

    task 6 fix lint

--- difference_calculator/scripts/gendiff.py
+++ difference_calculator/scripts/gendiff.py
@@ -2,89 +2,84 @@
 
 import argparse
 import json
+
 import yaml
 
+_SAME = "same"
+_SECOND = "second"
+_FIRST = "first"
 
-_SAME = 'same'
-_SECOND = 'second'
-_FIRST = 'first'
+
+def load(path):
+    ext = path.split(".")[-1]
+    match ext:
+        case "json":
+            return json.load(open(path))
+        case "yml" | "yaml":
+            return yaml.safe_load(open(path))
+        case _:
+            raise ValueError(f"unknown ext - {ext}")
+
+
+def value(value):
+    if isinstance(value, dict):
+        return diff(value, value)
+
+    return value
+
+
+def row(status, key, value):
+    return {"status": status, "key": key, "value": value}
+
+
+def diff(file1, file2):
+    keys = sorted(list(file1 | file2))
+    status = []
+
+    for key in keys:
+        value1 = file1.get(key)
+        value2 = file2.get(key)
+
+        if key in file1 and key in file2 and value1 == value2:
+            status.append(row(_SAME, key, value(value1)))
+        elif key in file1 and key in file2 and value1 != value2:
+            if isinstance(value1, dict) and isinstance(value2, dict):
+                status.append(row(_SAME, key, diff(value1, value2)))
+            else:
+                status.append(row(_FIRST, key, value(value1)))
+                status.append(row(_SECOND, key, value(value2)))
+        elif key in file1 and key not in file2:
+            status.append(row(_FIRST, key, value(value1)))
+        else:
+            status.append(row(_SECOND, key, value(value2)))
+
+    return status
 
 
 def formating(value, ind):
     if isinstance(value, bool):
         return str(value).lower()
     if value is None:
-        return 'null'
+        return "null"
     if isinstance(value, list):
         return stylish(value, ind)
     return value
 
 
-def load(path):
-    ext = path.split('.')[-1]
-    match ext:
-        case 'json':
-            return json.load(open(path))
-        case 'yml' | 'yaml':
-            return yaml.safe_load(open(path))
-        case _:
-            raise ValueError(f'unknown ext - {ext}')
-
-
-def diff(file1, file2):
-    keys = sorted(list(file1 | file2))
-    status = []
-    for key in keys:
-        if key in file1 and key in file2 and file1[key] == file2[key]:
-            if isinstance(file1[key], dict):
-                value = diff(file1[key], file2[key])
-            else:
-                value = file1[key]
-            status.append({"status": _SAME, "key": key, "value": value})
-        elif key in file1 and key in file2 and file1[key] != file2[key]:
-            if isinstance(file1[key], dict) and isinstance(file2[key], dict):
-                value = diff(file1[key], file2[key])
-                status.append({"status": _SAME, "key": key, "value": value})
-            elif isinstance(file1[key], dict):
-                value = diff(file1[key], file1[key])
-                status.append({"status": _FIRST, "key": key, "value": value})
-                status.append({"status": _SECOND, "key": key, "value": file2[key]})
-            elif isinstance(file2[key], dict):
-                value = diff(file2[key], file2[key])
-                status.append({"status": _FIRST, "key": key, "value": file1[key]})
-                status.append({"status": _SECOND, "key": key, "value": value})
-            else:
-                status.append({"status": _FIRST, "key": key, "value": file1[key]})
-                status.append({"status": _SECOND, "key": key, "value": file2[key]})
-        elif key in file1 and key not in file2:
-            if isinstance(file1[key], dict):
-                value = diff(file1[key], file1[key])
-            else:
-                value = file1[key]
-            status.append({"status": _FIRST, "key": key, "value": value})
-        else:
-            if isinstance(file2[key], dict):
-                value = diff(file2[key], file2[key])
-            else:
-                value = file2[key]
-            status.append({"status": _SECOND, "key": key, "value": value})
-    return status
-
-
 def stylish(status, ind=0):
-    draw = ['{']
+    draw = ["{"]
     for row in status:
-        match row['status']:
-            case 'same':
-                begin = '    '
-            case 'second':
-                begin = '  + '
-            case 'first':
-                begin = '  - '
-        value = formating(row['value'], ind + 1)
+        match row["status"]:
+            case "same":
+                begin = "    "
+            case "second":
+                begin = "  + "
+            case "first":
+                begin = "  - "
+        value = formating(row["value"], ind + 1)
         draw.append(f'{"    " * ind}{begin}{row["key"]}: {value}')
-    draw.append('    ' * ind + '}')
-    return '\n'.join(draw)
+    draw.append("    " * ind + "}")
+    return "\n".join(draw)
 
 
 def generate_diff(path1, path2):
@@ -95,15 +90,15 @@ def generate_diff(path1, path2):
 
 
 def show_certificate():
-    desc = 'Compares two configuration files and shows a difference.'
+    desc = "Compares two configuration files and shows a difference."
     parser = argparse.ArgumentParser(description=desc)
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
-    parser.add_argument('-f', '--format', help='set format of output')
+    parser.add_argument("first_file")
+    parser.add_argument("second_file")
+    parser.add_argument("-f", "--format", help="set format of output")
 
     args = parser.parse_args()
     print(generate_diff(args.first_file, args.second_file))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     show_certificate()
