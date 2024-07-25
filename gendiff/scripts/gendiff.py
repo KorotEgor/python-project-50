5bf949a81c1c9b644e251a34ac858b032ec75c87
korotkov_egor <korotkovegor10@gmail.com>
2024-07-25 18:33:26 +0300

    add json

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -58,6 +58,24 @@ def diff(file1, file2):
     return status
 
 
+def find_diff(data1, data2):
+    keys = sorted(set(data1.keys()).union(data2.keys()))
+    diff = {}
+
+    for key in keys:
+        if key in data1 and key not in data2:
+            diff[key] = ('removed', data1[key])
+        elif key not in data1 and key in data2:
+            diff[key] = ('added', data2[key])
+        elif data1[key] == data2[key]:
+            diff[key] = ('unchanged', data1[key])
+        elif isinstance(data1[key], dict) and isinstance(data2[key], dict):
+            diff[key] = ('nested', find_diff(data1[key], data2[key]))
+        else:
+            diff[key] = ('changed', (data1[key], data2[key]))
+    return json.dumps(diff, indent=4)
+
+
 def draw(status, style):
     match style:
         case "plain":
@@ -71,7 +89,12 @@ def draw(status, style):
 def generate_diff(path1, path2, style="stylish"):
     file1 = load(path1)
     file2 = load(path2)
+
+    if style == "json":
+        return find_diff(file1, file2)
+
     status = diff(file1, file2)
+
     return draw(status, style)
 
 
